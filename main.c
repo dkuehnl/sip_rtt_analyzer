46d2c87d61dfbd696d06b3f46ed77a8b28ca9ab3
Dennis Kühnlein (Dev) <d.kuehnlein@outlook.com>
2024-12-29 22:18:39 +0100

    finished main-structure: - all functions declared - main logic implemented - data-structure defined

--- main.c
+++ main.c
@@ -7,12 +7,49 @@
 #include <time.h>
 #include <sys/time.h>
 
-char* get_timestamp(){
-    struct timeval tv;
+typedef struct {
+    int request_number;
+    struct timeval starttime;
+    struct timeval endtime;
+    char delay[64];
+    char sip_response[248]
+} measurement_t;
+
+void evaluate_total_rtt(measurement_t *measurement, char *min, char *max, char *avg) {
+
+}
+
+void validate_sip_answer(measurement_t *measurement, char *buffer) {
+
+}
+
+void calculate_delay(measurement_t *measurement){
+
+}
+
+void print_progress_bar(int current, int total) {
+    int bar_width = 50; 
+    float progress = (float)current / total; 
+    int pos = bar_width * progress; 
+
+    printf("\r[");
+    for (int i = 0; i < bar_width; ++i){
+        if (i < pos) {
+            printf("=");
+        } else if (i == pos) {
+            printf(">"); 
+        } else {
+            printf(" ");
+        }
+    }
+    printf("] %d/%d Pakete", current, total);
+    fflush(stdout); 
+}
+
+char *get_printable_timestamp(struct timeval tv){
     struct tm *timeinfo;
     static char timeString[100];
     char msec_buffer[8];
-    gettimeofday(&tv, NULL);
     timeinfo = localtime(&tv.tv_sec);
 
     strftime(timeString, sizeof(timeString), "%d-%m-%Y %H:%M:%S", timeinfo);
@@ -22,7 +59,8 @@ char* get_timestamp(){
     return timeString;
 }
 
-int get_options_request(char *buffer, const char *proxy, const int paket_counter) {
+int get_options_request(char *buffer, const char *proxy, measurement_t *measurement) {
+    gettimeofday(&measurement->starttime, NULL);
     snprintf(buffer, 1024,
     "OPTIONS sip:%s SIP/2.0\r\n"
     "Via: SIP/2.0/TCP 192.168.178.62:5060;branch=z9hG4bK776asdhds\r\n"
@@ -37,7 +75,7 @@ int get_options_request(char *buffer, const char *proxy, const int paket_counter
     "Allow: INVITE, ACK, CANCEL, OPTIONS, BYE\r\n"
     "Content-Length: 0\r\n"
     "\r\n",
-    proxy, proxy, proxy, paket_counter, get_timestamp()
+    proxy, proxy, proxy, measurement->request_number, get_printable_timestamp(measurement->starttime)
     );
 
     if (strlen(buffer) > 0) {
@@ -105,7 +143,13 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int                 sockfd, rv; 
+    measurement_t *measurement = malloc(sender_count * sizeof(measurement_t));
+    if (measurement == NULL) {
+        fprintf(stderr, "Something went wrong with memory-allocation.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int                 sockfd; 
     struct sockaddr_in  sockaddr;
     char                buffer[1024];
 /*
@@ -125,17 +169,51 @@ int main(int argc, char *argv[]) {
         close(sockfd); 
         exit(EXIT_FAILURE);
     }*/
-
+    printf("Start measurement (%d request will be send)\n", sender_count);
     for (int i = 0; i < sender_count; i++) {
-        if (get_options_request(buffer, destination_ip,i) != 0) {
+        memset(&buffer, 0x00, sizeof(buffer)); 
+        print_progress_bar(i+1, sender_count);
+        measurement[i].request_number = i+1;
+
+        if (get_options_request(buffer, destination_ip, &measurement[i]) != 0) {
             fprintf(stderr, "Error while creating OPTIONS-Request occured.\n"); 
-            //shutdown(sockfd, SHUT_RDWR);
-            //close(sockfd); 
+            snprintf(measurement[i].sip_response, sizeof(measurement[i].sip_response), "Error");
+            shutdown(sockfd, SHUT_RDWR);
+            close(sockfd); 
             exit(EXIT_FAILURE);
         }
-        printf("%s", buffer); 
-        break;
-        //rv = write(sockfd, buffer, strlen(buffer));  
+
+        write(sockfd, buffer, strlen(buffer)); 
+        memset(&buffer, 0x00, sizeof(buffer)); 
+        read(sockfd, buffer, sizeof(buffer));
+        gettimeofday(&measurement[i].endtime, NULL);
+        validate_sip_answer(&measurement[i], buffer); 
+        calculate_delay(&measurement[i]); 
     }
+
+    char min[64]; 
+    char max[64];
+    char avg[64];
+    evaluate_total_rtt(measurement, min, max, avg); 
+
+    if (send_summary == 1) {
+        //Baue SIP-OPTION mit X-Headern 
+        //Begrenzung einbauen (<20 X-Header)
+        //Struktur X-Header: Paket 1 // Send: HH:MM:SS.mmm // Recv: HH:MM:SS.mmm // Delay: SS.mmm
+        //Letzter X-Header: Min-Delay: SS.mmm // Max-Delay: SS.mmm // Average Delay: SS.mmm
+    }
+    printf("\nSummary:\n"); 
+    printf("Tested Proxy: %s\tPakets send: %d\n", destination_ip, sender_count);
+    for (int i = 0; i < sender_count; i++) {
+        printf("Paket-Nr: %d\tSend: %s\tRecv: %s\tDelay: %s\tAnswer: %s\n", 
+        measurement[i].request_number,
+        get_printable_timestamp(measurement[i].starttime), 
+        get_printable_timestamp(measurement[i].endtime), 
+        measurement[i].delay, 
+        measurement[i].sip_response);
+    }
+    printf("===============================================================\n");
+    printf("Min-Delay: %s\tMax-Delay: %s\tAverage: %s\n", min, max, avg);
+
     return 0; 
 }
